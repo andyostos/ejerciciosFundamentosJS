d1d8a754661e8399dff619d63a95ae5cde9cabcb
andyostos <andres.ostos.art@gmail.com>
2024-01-08 18:09:29 -0600

    first commit

---
+++ ejerciciosJavaScript.js
@@ -0,0 +1,115 @@
+/** Ejercicio para esta sesion
+ * 
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+       -number
+*/
+let numUno = 1;
+let numDos = 2;
+let numTres = 3;
+let numCuatro = 4;
+let numCinco = 5;
+let numSeis = 6;
+let numSiete = 7;
+let numOcho = 8;
+let numNueve = 9;
+let numDiez = 10;
+
+
+/**      -string
+ */
+var colorAzul = 'Azul';
+var colorVerde = 'Verde';
+var colorRojo = 'Rojo';
+var colorAmarillo = 'Amarillo';
+var colorVioleta = 'Violeta';
+var colorMagenta = 'Magenta';
+var colorCian = 'Cian';
+var colorCafe = 'Cafe';
+var colorPurpura = 'Purpura';
+var colorNaranja = 'Naranja';
+/** 
+ *      -boolean
+ */
+let propiedadFuerte = false;
+let propiedadBelleza = true;
+let propiedadAgilidad = false;
+let propiedadInteligencia = true;
+let propiedadAstucia = true;
+let propiedadValor = true;
+let propiedadAnsiedad = true;
+let propiedadHambre = false;
+let propiedadVigor = false;
+let propiedadHonestidad = true;
+
+
+/** 
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+  */
+ var numeroA = 25;
+ var numeroB = 5;
+
+ let suma = numeroA + numeroB;
+ console.log(suma);
+ let resta = numeroA - numeroB;
+ console.log(resta);
+ let multiplicacion = numeroA * numeroB;
+ console.log(multiplicacion)
+ let division = numeroA / numeroB;
+ console.log(division)
+ /** 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+*/
+let varJuan = 'Juan ';
+let varTieneHambre = 'tiene hambre.';
+let varEjercicio1 = varJuan + varTieneHambre;
+console.log(varEjercicio1);
+
+let varElMosquito = 'El Mosquito ';
+let varNoDormir = 'no me deja dormir.';
+let varEjercicio2 = varElMosquito + varNoDormir;
+console.log(varEjercicio2);
+
+let varPlayStation = 'El Play Station ';
+let varExclusivos = 'tiene buenos exclusivos.';
+let varEjercicio3 = varPlayStation + varExclusivos;
+console.log(varEjercicio3);
+
+let varMiLibro = 'Mi libro favorito es ';
+let varTokyoBlues = 'Tokyo Blues.';
+let varEjercicio4 = varMiLibro + varTokyoBlues;
+console.log(varEjercicio4);
+
+let varFlores = 'Para mi las flores ';
+let varBlancasSon = 'blancas son hermosas.';
+let varEjercicio5 = varFlores + varBlancasSon;
+console.log(varEjercicio5);
+ 
+ /** 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ */
+let esteAño = 2024;
+let añoString = String(esteAño);
+
+let cantidadDeHijos = '3';
+let hijosNumber = Number(cantidadDeHijos);
+
+let eresHumano = true;
+let humanoNumber = Number(eresHumano);
+
+let cuantosPlanetasHayHumanos = 1;
+let planetaBoolean = Boolean(cuantosPlanetasHayHumanos);
+
+let cantidadDeTrajes = '7';
+let trajesNumber = Number(cantidadDeTrajes);
+
+
+
+ /**   CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
---
+++ variables.js
@@ -0,0 +1,86 @@
+//comentario de una linea
+/**
+ * Comentarios
+ * Multilinea
+ * en JS
+ */
+
+//Variable numerica
+
+var edadHector = 25;
+console.log("La edad de Hector es: " + edadHector);
+
+let deniseeYearOfBirth = 1995;
+console.log("Denisse was born in: " + deniseeYearOfBirth);
+
+let suma = edadHector + deniseeYearOfBirth;
+console.log("La suma de la edad de hctor y el año de naciemiento de Denisse es: " + suma)
+
+// Variables de tipo string, comillas dobles o simple
+
+let postreFavorito = "Helado";
+
+var movieFav = "Star Wars";
+
+// Variable tipo booleano, true or false
+
+let myNameIsDenisse = false;
+
+var amIAlive = true;
+
+//Variables de tipo null
+
+let datoVacio = null;
+let datoVacio1 = "";
+
+// Variable de tipo Arrey
+
+let coleccionDeCoches = ["Porsche", "Mercedes-Benz", "Ferrari", "Nissan"];
+let frutas = ["Manzana", "Plátano", "Mango"];
+let productos = ["higiene", "salud", "alimentos", "ropa"];
+
+let numerosPares = [0, 2, 4, 6, 8, 10];
+console.log(numerosPares);
+
+//Variable de tipo Objeto
+
+let libro = {
+    nombre: 'el mundo de sofia',
+    editorial: 'ABC',
+    año: '1990',
+    genero: 'filosofia',
+};
+
+const NOMBRE_USUARIO = 'Andy';
+
+// x = 1;
+// alert("x = " + x)
+// var x;
+
+// //var
+// var nombre = "Marcus";
+// console.log(nombre);
+
+// if (true){
+//     var nombre = 'Dominic';
+//     console.log(nombre)
+// };
+// console.log(nombre);
+
+// //let
+// let edad = 45;
+// console.log(edad);
+
+// if (true){
+//     let edad = 40;
+//     console.log(edad)
+// };
+// console.log(edad);
+
+let nombre = 'Alberto';
+let edad = 22;
+let condicion = true;
+
+let edadString = String(edad);
+let nombreNumerico = Number(nombre);
+let edadBooleano = Boolean(edad);
